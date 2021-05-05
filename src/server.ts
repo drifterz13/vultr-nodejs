d9e7093c1c49e201171b2bfd4d95bf430764073d
drifterz13 <mixmyxxe@gmail.com>
2021-05-05 22:04:29 +0700

    trigger ci

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 8000;
 //   .catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello world! 🌐🌎");
+  res.send("Hello world! 🌐🌎🚀");
 });
 
 app.listen(PORT, () => {
