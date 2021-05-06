6c9e7d48830659d969d6b0badeaf5875334045cd
drifterz13 <mixmyxxe@gmail.com>
2021-05-06 13:01:16 +0700

    try to connect with dokku mongo

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,24 @@
 import express from "express";
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-// mongoose
-//   .connect("mongodb://mongo:27017/vultr_test_db", {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useFindAndModify: false,
-//     useCreateIndex: true,
-//   })
-//   .then(() => console.log("Connected to mongodb 🌱."))
-//   .catch((err) => console.error(err));
+const MONGO_URL =
+  process.env.MONGO_URL || "mongodb://mongo:27017/vultr_test_db";
+
+mongoose
+  .connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("Connected to mongodb 🌱."))
+  .catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello world! 🌎🚀🌖");
+  res.send("Hello world! 🌎🚀🌖🌱");
 });
 
 app.listen(PORT, () => {
